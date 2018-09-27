b92dd410f1437c7bec7d26961ae7a1556bb521e5
MagicCurlyHair <g.shokanov@gmail.com>
2018-09-27 12:13:09 +0300

    function longestConsecutiveLength now functions properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const set = new Set();
+  let longest = 0;
+  let number;
+  let counter;
+  for (let i = 0, len = array.length; i < len; i++){
+    // check for consecutive numbers in set
+    number = array[i];
+    counter = 1;
+    while (set.has(number - 1)){
+      counter++;
+      number--;
+    }
+    number = array[i];
+    while (set.has(number + 1)){
+      counter++;
+      number++;
+    }
+    if (counter > longest){
+      longest = counter;
+    }
+    set.add(array[i]);
+  }
+  return longest;
 }
